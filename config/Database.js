90b5d867172c7e77811df5c1d32678239286e12e
Azda45 <72027480+Azda45@users.noreply.github.com>
2024-07-27 20:04:44 +0800

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('defaultdb', 'avnadmin', '', {
+const db = new Sequelize('defaultdb', 'avnadmin', 'AVNS_lZq8BPbCIbuq_zPDjnb', {
     host: 'mysql-2173c024-azdabtc1-91b7.l.aivencloud.com',
-    dialect: "mysql",
-    port:10844
+    port:10844,
+    dialect: "mysql"
 });
 
 export default db;
